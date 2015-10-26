f1abfc7efbd74a424c818e0a6bcddff3bfc7eb73
Jonathan Lee <jonlee50@hotmail.com>
2015-10-26 00:15:44 -0400

    Commit 3
    Added Reset R key.

---
+++ Fur Vs Flowers/Assets/_Scripts/LevelReset.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelReset : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.R)) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//Application.LoadLevel(Application.loadedLevel);
+		//if (Input.GetKeyDown(KeyCode.R)) {
+		//	Application.LoadLevel(Application.loadedLevel);
+		//}
+		//if (other.gameObject.CompareTag ("Player"))
+		//{
+		//
+		//	Application.LoadLevel(Application.loadedLevel);
+		//	Application.LoadLevel(0);
+		//
+		//}
+	}
+}
